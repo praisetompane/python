f665ffc56c768464fac8c1bd8644dd4fb5c3674f
Praise Tompane <praisetompane@icloud.com>
2025-03-23 12:03:25 +0200

    docs: object definition. clean up setup. pin to latest python.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+VERSION="3.13.2"
 echo "Adding asdf python plugin"
     asdf plugin add python
 echo "Done"
@@ -10,9 +11,17 @@ if [ "$ostype" = "Linux" ]; then
 fi
 
 echo "Installing python"
-    asdf install python latest
+    asdf install python $VERSION
 echo "Done"
 
 echo "Set python version"
-    asdf set python latest
+    asdf set python $VERSION
+echo "Done"
+
+echo "Install pipenv"
+    pip install pipenv --user
+echo "Done"
+
+echo "Adding user binaries to system path"
+    echo 'export PATH="$PATH:~/.local/bin"' >> ~/.zshrc
 echo "Done"
